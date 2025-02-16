c7c8bcc6f7657e20aadacdd9e2f69ee133a4dca4
Tymur <tymurkulivar@gmail.com>
2025-02-16 19:59:08 +0100

    add sql requirement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module es.codevert {
     requires javafx.fxml;
     requires java.desktop;
     requires com.github.ironbit;
+    requires java.sql;
 
     opens es.codevert to javafx.fxml;
     exports es.codevert;
