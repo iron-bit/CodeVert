a25c127fc6576858d0a7fffe08033ad2c8c85c72
RedondoDEV <javierRedondoDEV@gmail.com>
2025-02-17 23:07:40 +0100

    Second view progress but not showing db info in the left pane and checkCredentials and connectToDB for Clau

--- src/main/java/es/codevert/SecondaryController.java
+++ src/main/java/es/codevert/SecondaryController.java
@@ -1,20 +1,135 @@
 package es.codevert;
 
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import com.github.ironbit.FileExtension;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 public class SecondaryController {
 
 
     public TextField urlLabel, userLabel, passwordLabel;
+    public HBox elQueDesaparece;
+    public GridPane gridTocho;
+    public GridPane rightGrid;
+    public ImageView myGifPane;
 
     public void connectToDB(ActionEvent actionEvent) {
-        if (!urlLabel.getText().isBlank()) {
-
+        if (checkCredentials()) {
+            connectToDB();
+            handleContainerTransition();
+        } else {
+            changeErrorBorder();
         }
+    }
+
+    private boolean checkCredentials() {
+        // TODO comprobar si es correcto
+        return false;
+    }
+
+    private void connectToDB() {
+        // TODO connect to database
+        System.out.println("Connexion con db exitosa");
+    }
+
+    private void handleContainerTransition() {
+        Timeline delay;
+
+        delay = new Timeline(new KeyFrame(Duration.seconds(2.5), event -> {
+            vanishMainLayout(160, 5);
+
+            elQueDesaparece.setDisable(true);
+            gridTocho.setDisable(false);
+
+            makesConverterAppear(160, 10);
+        }));
+
+        delay.play();
+
+        moveDownGif(100, 15);
+    }
+
+    private void vanishMainLayout(int stepsOff, int durationOff) {
+        Timeline vanishTimeline = new Timeline();
+
+        for (int i = 0; i <= stepsOff; i++) {
+            KeyFrame keyFrame = new KeyFrame(
+                    Duration.millis(i * durationOff),
+                    event -> {
+                        double newOpacity = this.elQueDesaparece.getOpacity() - 1.0 / stepsOff;
+                        changePaneOpacity(this.elQueDesaparece, newOpacity);
+                    }
+
+            );
+            vanishTimeline.getKeyFrames().add(keyFrame);
+        }
+        vanishTimeline.play();
+    }
+
+    private void makesConverterAppear(int stepsOn, int durationOn) {
+        Timeline appearTimeline = new Timeline();
+
+        for (int i = 0; i <= stepsOn; i++) {
+            KeyFrame keyFrame = new KeyFrame(
+                    Duration.millis(i * durationOn),
+                    event -> {
+                        double newOpacity = this.gridTocho.getOpacity() + 1.0 / stepsOn;
+                        changePaneOpacity(this.gridTocho, newOpacity);
+                    }
+            );
+            appearTimeline.getKeyFrames().add(keyFrame);
+        }
+        appearTimeline.play();
+    }
+
+    private void changePaneOpacity(Pane pane, double value) {
+        pane.setOpacity(value);
+    }
+
+    private void moveDownGif(int stepsDown, int durationDown) {
+        Timeline moveDownTimeline = new Timeline();
+
+        for (int i = 0; i <= stepsDown; i++) {
+            KeyFrame keyFrame = new KeyFrame(
+                    Duration.millis(i * durationDown),
+                    event -> {
+                        double newPosition = this.myGifPane.getTranslateY() + 1;
+                        myGifPane.setTranslateY(newPosition);
+                    }
+            );
+            moveDownTimeline.getKeyFrames().add(keyFrame);
+        }
+        moveDownTimeline.play();
+    }
+
+    public void changeErrorBorder() {
+        urlLabel.setStyle("-fx-border-color: red");
+        userLabel.setStyle("-fx-border-color: red");
+        passwordLabel.setStyle("-fx-border-color: red");
+    }
+
+    public void returnOriginalBorder(KeyEvent keyEvent) {
+        urlLabel.setStyle("-fx-border-color: none");
+        userLabel.setStyle("-fx-border-color: none");
+        passwordLabel.setStyle("-fx-border-color: none");
     }
 }
